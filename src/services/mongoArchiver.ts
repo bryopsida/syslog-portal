edc8c9e36ac19633e16a5b8b1f7cd4ed2ae4584e
bryopsida <8363252+bryopsida@users.noreply.github.com>
2022-12-31 10:48:53 -0600

    wire dependencies and hooks

--- src/services/mongoArchiver.ts
+++ src/services/mongoArchiver.ts
@@ -1,8 +1,31 @@
-import { injectable } from 'inversify'
-import { ILogMessage, ILogMessageListener } from '../interfaces/server'
+import { injectable, inject, preDestroy, postConstruct } from 'inversify'
+import { ILogMessage, ILogMessageListener, IServer } from '../interfaces/server'
+import { TYPES } from '../types'
+import { Logger } from 'pino'
 
 @injectable()
 export class MongoArchiver implements ILogMessageListener {
+  private readonly server: IServer
+  private readonly log: Logger
+
+  constructor(
+    @inject(TYPES.Services.Server) server: IServer,
+    @inject(TYPES.Logger) log: Logger
+  ) {
+    this.server = server
+    this.log = log
+  }
+
+  @postConstruct()
+  setup() {
+    this.server.onLogMessage(this)
+  }
+
+  @preDestroy()
+  cleanUp() {
+    this.server.offLogMessage(this)
+  }
+
   onLogMessage(message: ILogMessage): Promise<any> {
     throw new Error('Method not implemented.')
   }
